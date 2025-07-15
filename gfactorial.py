959f2d39307ee2f8e1f5b6ca1d880d7d9b5a5123
Amed Reyes <reyesrodriguezjaime@gmail.com>
2025-07-15 15:47:16 -0600

    refactor

--- gfactorial.py
+++ gfactorial.py
@@ -4,16 +4,16 @@ License: Corresponding copyright holders
 Date: July 11th, 2025
 '''
 def gfactorial(x):
-    if x < 0:
-        raise ValueError("Error: factorial is not defined for negative numbers!")
-    else:
-        if x == 0:
-            return 1
-        else:
-            return x * gfactorial(x-1)      # Copilot fixed a bug here (missing x factor)
+	if x < 0:
+		raise ValueError("Error: factorial is not defined for negative numbers!")
+	else:
+		if x == 0:
+			return 1
+		else:
+			return x * gfactorial(x-1)      # Copilot fixed a bug here (missing x factor)
 
 if __name__ == '__main__':
-    import sys
-    param = int(sys.argv[1])
-    print(" > Calculating factorial for " + str(param))
-    print(" > gfactorial(" + str(param) + ") = " + str(gfactorial(param)))
+	import sys
+	param = int(sys.argv[1])
+	print(" > Calculating factorial for " + str(param))
+	print(" > gfactorial(" + str(param) + ") = " + str(gfactorial(param)))
